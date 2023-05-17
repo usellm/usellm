de3f882ea417fffc3abc96722ea48faf718be7e4
Aakash N S <aakashns@jovian.ai>
2023-05-17 12:32:46 +0530

    Add todo pages for sidebar elements (#8)

--- usellm.org/config/docs.ts
+++ usellm.org/config/docs.ts
@@ -28,12 +28,122 @@ export const docsConfig: DocsConfig = {
         },
         {
           title: "Installation",
-          href: "/docs",
+          href: "/docs/installation",
           items: [],
         },
         {
           title: "Usage",
-          href: "/docs",
+          href: "/docs/usage",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "API Reference",
+      items: [
+        {
+          title: "useLLM Hook",
+          href: "/docs/api-reference/usellm-hook",
+          items: [],
+        },
+        {
+          title: "createLLMService",
+          href: "/docs/api-reference/create-llm-service",
+          items: [],
+        },
+        {
+          title: "registerTemplate",
+          href: "/docs/api-reference/register-template",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "Guides",
+      items: [
+        {
+          title: "Message Streaming",
+          href: "/docs/guides/message-streaming",
+          items: [],
+        },
+        {
+          title: "Prompt Engineering",
+          href: "/docs/guides/prompt-engineering",
+          items: [],
+        },
+        {
+          title: "Conversation History",
+          href: "/docs/guides/conversation-history",
+          items: [],
+        },
+        {
+          title: "Embedding Documents",
+          href: "/docs/guides/embedding-documents",
+          items: [],
+        },
+        {
+          title: "User Authentication",
+          href: "/docs/guides/user-authentication",
+          items: [],
+        },
+        {
+          title: "Rate Limiting",
+          href: "/docs/guides/rate-limiting",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "Examples",
+      items: [
+        {
+          title: "Next.js (Pages Router)",
+          href: "/docs/examples/nextjs-pages-router",
+          items: [],
+        },
+        {
+          title: "Next.js (App Router)",
+          href: "/docs/examples/nextjs-app-router",
+          items: [],
+        },
+        {
+          title: "React Native (Android/iOS)",
+          href: "/docs/examples/react-native-android-ios",
+          items: [],
+        },
+        {
+          title: "Chrome Extension",
+          href: "/docs/examples/chrome-extension",
+          items: [],
+        },
+        {
+          title: "Slack Bot",
+          href: "/docs/examples/slack-bot",
+          items: [],
+        },
+        {
+          title: "Discord Bot",
+          href: "/docs/examples/discord-bot",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "Contribute",
+      items: [
+        {
+          title: "Development Setup",
+          href: "/docs/contribute/development-setup",
+          items: [],
+        },
+        {
+          title: "Deployment & Publishing",
+          href: "/docs/contribute/deployment-and-publishing",
+          items: [],
+        },
+        {
+          title: "Credits",
+          href: "/docs/contribute/credits",
           items: [],
         },
       ],
