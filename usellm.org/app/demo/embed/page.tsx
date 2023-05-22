76bd631da2c7ca5d50a180dfdb5530c73fb981b5
Aakash N S <aakashns@jovian.ai>
2023-05-22 18:47:52 +0530

    fix styles

--- usellm.org/app/demo/embed/page.tsx
+++ usellm.org/app/demo/embed/page.tsx
@@ -16,8 +16,7 @@ export default function EmbedDemoPage() {
     });
 
     const top3paragraphs = llm
-      .scoreEmbeddings({ embeddings, query: queryEmbeddings[0] })
-      .slice(0, 3)
+      .scoreEmbeddings({ embeddings, query: queryEmbeddings[0], top: 3 })
       .map(({ index }) => paragraphs[index])
       .join("\n\n");
 
@@ -29,7 +28,7 @@ export default function EmbedDemoPage() {
   }
 
   return (
-    <div>
+    <div style={{ padding: 8 }}>
       <textarea
         maxLength={2000}
         rows={10}
