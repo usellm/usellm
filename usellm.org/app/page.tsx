1071048b899c0e11f5d1302b5bf4b0a67c78b5e6
Aakash N S <aakashns@jovian.ai>
2023-05-22 15:03:06 +0530

    Add latest blog link on home page

--- usellm.org/app/page.tsx
+++ usellm.org/app/page.tsx
@@ -10,12 +10,23 @@ import {
 } from "@/components/page-header";
 import { StyleSwitcher } from "@/components/style-switcher";
 import { HomePageDemo } from "@/components/home-page-demo";
+import { Separator } from "@/components/ui/separator";
+import { ChevronRight } from "lucide-react";
 
 export default function IndexPage() {
   return (
     <div className="container relative pb-10">
       <StyleSwitcher />
       <PageHeader>
+        <Link
+          href="https://usellm.substack.com/p/release-notes-usellm-v020"
+          target="_blank"
+          className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
+        >
+          🚀 v0.2.0 <Separator className="mx-2 h-4" orientation="vertical" />
+          Audio recording, speech to text, and more..
+          <ChevronRight className="ml-1 h-4 w-4" />
+        </Link>
         <PageHeaderHeading>
           React Hooks for Large Language Models
         </PageHeaderHeading>
