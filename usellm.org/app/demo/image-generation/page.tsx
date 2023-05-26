c75a1814b49861e6b6b2d46003c2c7813d684312
Aakash N S <aakashns@jovian.ai>
2023-05-26 21:50:02 +0530

    fix more stuff

--- usellm.org/app/demo/image-generation/page.tsx
+++ usellm.org/app/demo/image-generation/page.tsx
@@ -1,39 +1,46 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import useLLM from "@/usellm";
+import useLLM from "usellm";
 import React, { useState } from "react";
 
 export default function ImageGeneration() {
   const [prompt, setPrompt] = useState("");
   const [image, setImage] = useState("");
+  const [status, setStatus] = useState("");
   const llm = useLLM({ serviceUrl: "/api/llm" });
 
   async function handleGenerateClick() {
+    setStatus("Generating...");
     setImage("");
     const { images } = await llm.generateImage({ prompt });
     setImage(images[0]);
+    setStatus("");
   }
 
   return (
-    <div className="max-w-4xl w-full mx-auto my-4">
-      <h1 className="font-medium text-4xl text-center">Image Generation</h1>
-      <div className="flex mt-4">
-        <Input
+    <div className="p-4 overflow-y-auto">
+      <h2 className="font-medium text-3xl">Image Generation</h2>
+      <div className="flex my-4">
+        <input
+          className="p-2 border rounded mr-2 w-full"
           type="text"
           placeholder="Enter a prompt here"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
         />
-        <Button onClick={handleGenerateClick} className="ml-2">
+        <button
+          className="p-2 border rounded bg-gray-100 hover:bg-gray-200 active:bg-gray-300 ml-2"
+          onClick={handleGenerateClick}
+        >
           Generate
-        </Button>
+        </button>
       </div>
+
+      {status && <div>{status}</div>}
 
       {image && (
         // eslint-disable-next-line @next/next/no-img-element
         <img
-          className="mt-4"
+          className="mt-4 rounded"
           src={image}
           alt={prompt}
           width={256}
