9d8173e760e98f1e50dc6785d55bdee19edab42e
Aakash N S <aakashns@jovian.ai>
2023-05-26 17:51:41 +0530

    Fix main demo

--- usellm.org/app/demo/page.tsx
+++ usellm.org/app/demo/page.tsx
@@ -1,9 +1,9 @@
-import { AIChatBotDemo } from "@/components/demos/ai-chatbot-demo";
+import AIChatBot from "./ai-chatbot/page";
 
 export default function DemoPage() {
   return (
     <div className="max-w-4xl w-full mx-auto p-4">
-      <AIChatBotDemo />
+      <AIChatBot />
     </div>
   );
 }
