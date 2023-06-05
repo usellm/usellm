e1a9afe626cb5e1a6ff17c76597a80cd0dab483a
Aakash N S <aakashns@jovian.ai>
2023-06-05 15:15:11 +0530

    Add twitter config

--- usellm.org/app/layout.tsx
+++ usellm.org/app/layout.tsx
@@ -50,6 +50,13 @@ export const metadata: Metadata = {
       },
     ],
   },
+  twitter: {
+    card: "summary_large_image",
+    title: `${siteConfig.name} - React Hooks for Large Language Models`,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@usellmteam",
+  },
 };
 
 interface RootLayoutProps {
