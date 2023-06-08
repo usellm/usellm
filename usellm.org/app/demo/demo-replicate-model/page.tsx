d8844dfcaacbb006db2c26d6f8f78db7760e384a
Biraj <birajde9@gmail.com>
2023-06-08 18:05:01 +0530

    Added a User Input for Wait Time

--- usellm.org/app/demo/demo-replicate-model/page.tsx
+++ usellm.org/app/demo/demo-replicate-model/page.tsx
@@ -15,12 +15,14 @@ export default function DemoReplicateModel() {
   const [modelId, setModelId] = useState(
     "c49dae362cbaecd2ceabb5bd34fdb68413c4ff775111fea065d259d577757beb"
   );
+  const [time, setTime] = useState("10");
 
   async function handleClick() {
     setResult("");
     const response = await llm.callReplicate({
       modelId: modelId,
       input: { prompt: input },
+      time: parseInt(time) * 1000,
     });
     console.log(response);
     setResult(response.output);
@@ -31,6 +33,9 @@ export default function DemoReplicateModel() {
       <h2 className="text-2xl font-semibold mb-4">
         Replicate Stable LM Model Demo
       </h2>
+      <label className="block mb-2">
+        <b>Replicate Model ID</b>
+      </label>
       <input
         className="p-2 border rounded mr-2 w-full mb-4 block dark:bg-gray-900 dark:text-white"
         type="text"
@@ -38,6 +43,19 @@ export default function DemoReplicateModel() {
         value={modelId}
         onChange={(e) => setModelId(e.target.value)}
       />
+      <label className="block mb-2">
+        <b>Waiting Time (in seconds)</b>
+      </label>
+      <input
+        className="p-2 border rounded mr-2 w-full mb-4 block dark:bg-gray-900 dark:text-white"
+        type="text"
+        placeholder="Enter Time in Seconds"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      />
+      <label className="block mb-2">
+        <b>Prompt</b>
+      </label>
       <input
         className="p-2 border rounded mr-2 w-full mb-4 block dark:bg-gray-900 dark:text-white"
         type="text"
@@ -45,6 +63,7 @@ export default function DemoReplicateModel() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
+
       <button
         className="p-2 border rounded bg-gray-100 hover:bg-gray-200 active:bg-gray-300 dark:bg-white dark:text-black font-medium"
         onClick={handleClick}
