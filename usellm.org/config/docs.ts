62b187f264ecd641465e3d420f2707d152612521
Harshit Agarwal <harshitagarwal@Harshits-MacBook-Air.local>
2023-06-09 18:01:34 +0530

    Added documentation of several function of server side

--- usellm.org/config/docs.ts
+++ usellm.org/config/docs.ts
@@ -157,8 +157,32 @@ export const docsConfig: DocsConfig = {
           href: "docs/api-reference/llmservice-chat",
         },
         {
+          title: "`.transcribe`",
+          href: "docs/api-reference/llmservice-transcribe",
+        },
+        {
+          title: "`.speak`",
+          href: "docs/api-reference/llmservice-speak",
+        },
+        {
+          title: "`.voiceChat`",
+          href: "docs/api-reference/llmservice-voiceChat",
+        },
+        {
           title: "`.embed`",
           href: "docs/api-reference/llmservice-embed",
+        },
+        {
+          title: "`.generateImage`",
+          href: "docs/api-reference/llmservice-generateImage",
+        },
+        {
+          title: "`.editImage`",
+          href: "docs/api-reference/llmservice-editImage",
+        },
+        {
+          title: "`.imageVariation`",
+          href: "docs/api-reference/llmservice-imageVariation",
         },
       ],
     },
