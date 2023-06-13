b177e39ef3e3d55d5e116db16a7665e936f54cc1
Harshit Agarwal <harshitagarwal@Harshits-MacBook-Air.local>
2023-06-13 19:57:28 +0530

    Added documentation of cloneVoice

--- usellm.org/config/docs.ts
+++ usellm.org/config/docs.ts
@@ -132,6 +132,10 @@ export const docsConfig: DocsConfig = {
           href: "docs/api-reference/call-huggingface",
         },
         {
+          title: "`.cloneVoice`",
+          href: "docs/api-reference/cloneVoice",
+        },
+        {
           title: "`<LLMProvider/>`",
           href: "docs/api-reference/llm-provider",
         },
@@ -187,6 +191,10 @@ export const docsConfig: DocsConfig = {
         {
           title: "`.imageVariation`",
           href: "docs/api-reference/llmservice-imageVariation",
+        },
+        {
+          title: "`.cloneVoice`",
+          href: "docs/api-reference/llmservice-cloneVoice",
         },
       ],
     },
